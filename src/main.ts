6b6111da947ee62bbbe51d61c198b94f70310ef5
21Savage <tursunboyevx43@gmail.com>
2025-05-02 16:42:59 +0500

    validationError was caught in this project

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { LoggerMiddleware } from "./middleware";
-import { HttpExceptionFilter } from "./filters";
-import { ValidationPipe } from "@nestjs/common";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // app.useGlobalFilters(new HttpExceptionFilter());
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
+    exceptionFactory(errors) {
+      let errorMessage = '';
+      errors.forEach((error) => {
+        if (error.constraints) {
+          errorMessage += `${error.property} has wrong value ${error.value}, ${Object.values(error.constraints).join(', ')}\n`;
+        }
+      });
+      return new BadRequestException(errorMessage.trim());
+    }
+    
+    
   }));
   
 
