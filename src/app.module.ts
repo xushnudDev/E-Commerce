f4294196dc1e859c2866561703d1c6a9481f49a5
21Savage <tursunboyevx43@gmail.com>
2025-05-02 17:24:54 +0500

    logging interceptor was created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CategoryModule, ProductModule, UserModule } from "./modules";
-import { APP_FILTER } from "@nestjs/core";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
 import { HttpExceptionFilter } from "./filters";
+import { LogginInterceptor } from "./interceptors/logging.interceptor";
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -12,7 +13,12 @@ import { HttpExceptionFilter } from "./filters";
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LogginInterceptor
     }
   ]
+  
 })
 export class AppModule {}
